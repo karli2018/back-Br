9e7277bff520300f397a5931346df7dd45d6484e
karli2018 <karli2018u@gmail.com>
2023-05-21 15:37:18 -0500

    feat(main):add conexion

--- database/database.js
+++ database/database.js
@@ -1,6 +1,14 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  host: process.env.MYSQLHOST,
-  dialect: "mysql",
-});
+// export const sequelize = new Sequelize("pruebabr","root","1234", {
+//   // host: process.env.MYSQLHOST,
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+export const sequelize = new Sequelize('colegiobr', 'Bris4s', 'Prueb4sl', {
+  dialect: 'mssql',
+  host: 'localhost'
+  }
+);
+
